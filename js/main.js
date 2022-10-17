2e99d8b07ad3f969fb03dbef61d722f8ebe40c52
Olegsander78 <onkotov@gmail.com>
2022-10-17 16:42:46 +0500

    Created Shining star

--- js/main.js
+++ js/main.js
@@ -14,23 +14,50 @@ function createCamera(scene){
 
 function createLight(scene){
     const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
+    light.intensity = 0.5;
+    light.groundColor = new BABYLON.Color3(0,0,1);
 }
 
 function createSun(scene){
+    const sunMaterial = new BABYLON.StandardMaterial('sunMaterial', scene);
+    sunMaterial.emissiveTexture = new BABYLON.Texture('assets/images/sun.jpg',scene);
+    sunMaterial.diffuseColor = BABYLON.Color3.Black();
+    sunMaterial.specularColor = BABYLON.Color3.Black();
+
     const sun = BABYLON.MeshBuilder.CreateSphere('sun',{
         segments: 16,
         diameter: 4
     }, scene);
+    sun.material = sunMaterial;
+
+    const sunLight = new BABYLON.PointLight('sunLight', BABYLON.Vector3.Zero(), scene);
+    sunLight.intensity = 2;
+}
+
+function createPlanet(scene){
+    const planetMaterial = new BABYLON.StandardMaterial('planetMaterial',scene);
+    planetMaterial.diffuseTexture = new BABYLON.Texture('assets/images/sand.png', scene);
+    planetMaterial.specularColor = BABYLON.Color3.Black();
+    
+    const planet = BABYLON.MeshBuilder.CreateSphere('planet',{
+        segments: 16,
+        diameter: 1
+    }, scene);
+    planet.position.x = 4;
+    planet.material = planetMaterial;
 }
 
 function createScene(){
     const scene = new BABYLON.Scene(engine);
+    scene.clearColor = BABYLON.Color3.Black();
 
     createCamera();
 
     createLight(scene);
 
     createSun(scene);
+
+    createPlanet(scene);
 
     return scene;
 
